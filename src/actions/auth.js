7acab762cf909daf3c5d14d8b4708db7231e1f7c
laginja <matko.smoljan@gmail.com>
2019-12-17 12:57:28 +0100

    Storing user data into firebase database

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,8 +1,14 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase';
+import database from '../firebase/firebase';
 
 export const startLogin = () => {
     /* Return a promise. You could also return a function here and then call it from the login component */
-    return firebase.auth().signInWithPopup(googleAuthProvider);
+    return firebase.auth().signInWithPopup(googleAuthProvider).then((auth) => {
+        const { uid, displayName, photoURL, email } = auth.user
+        const user = { displayName, photoURL, email}
+        database.ref(`users/${uid}`).set(user)
+        
+    });
 }
 
 export const startLogout = () => {
